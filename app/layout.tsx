13a204a7fd6884cd5183d1524eb33b3ff1894ea0
Birannchi Kulesika <biranchi.dev@proton.me>
2026-08-30 05:51:20 +0530

    fix(gtm): place GTM script in HTML head for synchronous scanner detection (#45) (#46)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata, Viewport } from 'next';
 import { Space_Grotesk, Newsreader } from 'next/font/google';
-import Script from 'next/script';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css';
 import { rootMetadata, websiteJsonLd, safeJsonLd } from '@/lib/seo';
@@ -41,6 +40,21 @@ export default function RootLayout({
       lang="en"
       className={`${spaceGrotesk.variable} ${newsreader.variable}`}
     >
+      <head>
+        {gtmId ? (
+          /* eslint-disable-next-line @next/next/next-script-for-ga */
+          <script
+            id="google-tag-manager"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer',${JSON.stringify(gtmId)});`,
+            }}
+          />
+        ) : null}
+      </head>
       <body className="min-h-screen bg-night text-paper antialiased">
         {gtmId ? (
           <noscript>
@@ -51,19 +65,6 @@ export default function RootLayout({
               style={{ display: 'none', visibility: 'hidden' }}
             />
           </noscript>
-        ) : null}
-        {gtmId ? (
-          <Script
-            id="google-tag-manager"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer',${JSON.stringify(gtmId)});`,
-            }}
-          />
         ) : null}
         <script
           type="application/ld+json"
